3c52d9dff22028fb52bfd766d6977db10c93918e
lantoniomxnusd <luisoteacher@gmail.com>
2025-03-19 21:58:03 -0600

    finished

--- week02/bom.js
+++ week02/bom.js
@@ -7,7 +7,17 @@ button.addEventListener('click', () => {
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
 
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus(); 
+        });
 
+        input.focus();
+        input.value = '';
 
     }
 });
