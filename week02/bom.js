6bb851fae2abef5d012b8551a6c08829bca84745
lantoniomxnusd <luisoteacher@gmail.com>
2025-03-19 21:57:24 -0600

    event listener, list, and delete button

--- week02/bom.js
+++ week02/bom.js
@@ -1,7 +1,13 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('#list')
+const list = document.querySelector('#list');
 
-button.addEventListener('click', function() {...});
-button.addEventListener('click'() => {...});
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
+
+
+    }
+});
