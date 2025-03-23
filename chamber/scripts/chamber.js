e96383f454022cb0aa7f2853f0c2d4706d1fea79
lantoniomxnusd <luisoteacher@gmail.com>
2025-03-23 16:45:59 -0600

    added the hamburger menu and made the javascript adjustments to open and close

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,17 @@
+const today = document.querySelector("#lastModified"); 
+const options = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+};
+
+today.innerHTML = "Last modified: " + new Date().toLocaleDateString("en-US", options);
+
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
+
