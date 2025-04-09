3870fd94f49a554a7658a1ad6ca250d3ec414873
lantoniomxnusd <luisoteacher@gmail.com>
2025-04-08 22:26:35 -0600

    added the json and javascript.

---
+++ scripts/links.js
@@ -0,0 +1,34 @@
+const baseURL = "https://lantoniomxnusd.github.io/wdd230/";
+const linksURL= "https://raw.githubusercontent.com/lantoniomxnusd/wdd230/main/data/links.json";
+const activities = document.querySelector('#activities');
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    // console.log(data);
+    displayLinks(data.weeks);
+  }
+
+  getLinks();
+
+  const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+      let section = document.createElement('section');
+      let heading = document.createElement('h3');
+      heading.textContent = week.week;
+  
+      week.links.forEach((link) => {
+        let linkElement = document.createElement('a');
+        linkElement.setAttribute('href', baseURL + link.url);
+        linkElement.textContent = link.title;
+        linkElement.setAttribute('target', '_blank');
+  
+        let listItem = document.createElement('p');
+        listItem.appendChild(linkElement);
+        section.appendChild(listItem);
+      });
+      
+      section.prepend(heading);
+      activities.appendChild(section);
+    });
+  };
--- styles/links.js
+++
@@ -1,2 +0,0 @@
-const baseURL = "https://lantoniomxnusd.github.io/wdd230/";
-const linksURL= "https://github.com/lantoniomxnusd/wdd230/data/links.json";
